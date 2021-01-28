aa2b51a483798f4e938e46de01239323ca944908
CassBut <clhurlbut@gmail.com>
2021-01-28 13:20:41 -0800

    initial file structure setup for MVC

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,8 +9,18 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//make the public folder available
+app.use(express.static(path.join(__dirname, 'public')));
+
 // turn on routes
 app.use(routes);
+
+//telling the app we want to use the handlebars template engine 
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
